a09d7a8344ada6c9dbe7863b29e7c60849b84602
Scheepers <scheepers.debruin@gmail.com>
2018-03-26 01:55:14 +0200

    Added collective service shell

--- core/custom/services/TmiCollectiveService.js
+++ core/custom/services/TmiCollectiveService.js
@@ -21,16 +21,6 @@ class TmiCollectiveService extends RestfulService {
    * @inheritDoc
    */
   get(request, response){
-    switch(true){
-      case request.header('Content-Type') == 'application/json;schema':
-      case request.user.isAdministrator: return super.get(request,response)
-      case request.user.isAuthenticated:
-        return super.get(request, response).map(
-          (subject) => this.privacyFilter(request.user, subject)
-        )
-      case request.user.isAnonymous: throw Service.FORBIDDEN
-      default: throw Service.INVALID
-    }
   }
 
   /**
@@ -38,12 +28,6 @@ class TmiCollectiveService extends RestfulService {
    * @inheritDoc
    */
   post(request, response){
-    switch(true){
-      case request.user.isAdministrator:
-      case request.user.isAnonymous: return super.post(request, response)
-      case request.user.isAnonymous: throw Service.FORBIDDEN
-      default: throw Service.INVALID
-    }
   }
 
   /**
@@ -51,13 +35,6 @@ class TmiCollectiveService extends RestfulService {
    * @inheritDoc
    */
   put(request, response){
-    switch(true){
-      case request.user.isAdministrator:
-      case request.user.isAuthenticated && this.isOwnCollective(request):
-        return super.put(request, response)
-      case request.user.isAnonymous: throw Service.FORBIDDEN
-      default: throw Service.INVALID
-    }
   }
 
   /**
@@ -65,13 +42,6 @@ class TmiCollectiveService extends RestfulService {
    * @inheritDoc
    */
   patch(request, response){
-    switch(true){
-      case request.user.isAdministrator:
-      case request.user.isAuthenticated && this.isOwnCollective(request):
-        return super.put(request, response)
-      case request.user.isAnonymous: throw Service.FORBIDDEN
-      default: throw INVALID
-      }
   }
 
   /**
@@ -79,13 +49,6 @@ class TmiCollectiveService extends RestfulService {
    * @inheritDoc
    */
   delete(request, response){
-    switch(true){
-      case request.user.isAdministrator:
-      case request.user.isAuthenticated && this.isOwnCollective(request):
-        return super.delete(request, response)
-      case request.user.isAnonymous: throw Service.FORBIDDEN
-      default: throw Service.INVALID
-    }
   }
 }
 
