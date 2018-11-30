25a36d3fcfb3fe6aa19ef511e9c394f0d11331e8
Scheepers <scheepers.debruin@gmail.com>
2018-11-30 11:32:08 +0200

    Splitting out Submodules

--- people/endpoints/post/post.stash.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file PostStash.js
- * Post Stash.
- */
-"use strict"
-
-
-module.exports = class PostStash extends core.stashes.MemoryStash {}
--- people/endpoints/profile/profile.stash.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file ProfileStash.js
- * Profile Stash.
- */
-"use strict"
-
-
-module.exports = class ProfileStash extends core.stashes.MemoryStash {}
